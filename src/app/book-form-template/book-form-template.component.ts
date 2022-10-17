92379e98e9524305af5536078d9abc854c86c8c7
Ben McMullin <46494991+benmcmullin@users.noreply.github.com>
2022-10-17 15:09:03 -0500

    ADD: Error in Empty Input

--- src/app/book-form-template/book-form-template.component.ts
+++ src/app/book-form-template/book-form-template.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { NgModule } from '@angular/core';
 
 @Component({
   selector: 'app-book-form-template',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookFormTemplateComponent implements OnInit {
 
+  formHasBeenSubmitted = false;
+
+  inputDetails = {
+    title: "",
+    author: "",
+    genre: "",
+  }
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onFormSubmit(formObj: NgForm) {
+    console.log("Submitted", formObj)
+    this.formHasBeenSubmitted= true;
+    this.inputDetails.title = formObj.value.title;
+    this.inputDetails.author = formObj.value.author;
+    this.inputDetails.genre = formObj.value.genre;
+    formObj.reset();
   }
 
 }
